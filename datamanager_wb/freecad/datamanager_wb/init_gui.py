f13c617e2f03e3a6f379825cdff99c2c6951acca
Roy Wright <roy@wright.org>
2026-01-07 16:30:42 -0600

    Split init_gui into thin bootstrap
    - Move DataManagerWorkbench into workbench.py
    
    - Keep init_gui focused on startup wiring
    
    - Exclude workbench.py from strict mypy (GUI entrypoint)

--- datamanager_wb/freecad/datamanager_wb/init_gui.py
+++ datamanager_wb/freecad/datamanager_wb/init_gui.py
@@ -2,18 +2,16 @@
 FreeCAD datamanager workbench
 """
 
-import os
-
 import FreeCAD as App
 import FreeCADGui as Gui
 
 from .commands import register_commands
 from .freecad_version_check import check_python_and_freecad_version
 from .main_panel import get_main_panel
-from .resources import ICONPATH, TRANSLATIONSPATH
+from .resources import TRANSLATIONSPATH
+from .workbench import DataManagerWorkbench
 
 translate = App.Qt.translate
-QT_TRANSLATE_NOOP = App.Qt.QT_TRANSLATE_NOOP
 
 # Add translations path
 Gui.addLanguagePath(TRANSLATIONSPATH)
@@ -24,50 +22,6 @@ check_python_and_freecad_version()
 
 
 register_commands(get_main_panel)
-
-
-class DataManagerWorkbench(Gui.Workbench):
-    """
-    class which gets initiated at startup of the gui
-    """
-
-    MenuText = translate("Workbench", "data manager workbench")
-    ToolTip = translate("Workbench", "a simple data manager workbench")
-    Icon = os.path.join(ICONPATH, "datamanager_wb.svg")
-    toolbox = [
-        "DataManagerVarsetManagement",
-        "DataManagerAliasManagement",
-    ]
-
-    def GetClassName(self):
-        """
-        Return the class name of the workbench
-        """
-        return "Gui::PythonWorkbench"
-
-    def Initialize(self):
-        """
-        This function is called at the first activation of the workbench.
-        here is the place to import all the commands
-        """
-
-        App.Console.PrintMessage(translate("Log", "Switching to datamanager_wb") + "\n")
-
-        # NOTE: Context for this commands must be "Workbench"
-        self.appendToolbar(QT_TRANSLATE_NOOP("Workbench", "Data Manager"), self.toolbox)
-        self.appendMenu(QT_TRANSLATE_NOOP("Workbench", "Data Manager"), self.toolbox)
-
-    def Activated(self):
-        """
-        code which should be computed when a user switch to this workbench
-        """
-        App.Console.PrintMessage(translate("Log", "Workbench datamanager_wb activated. ;-)") + "\n")
-
-    def Deactivated(self):
-        """
-        code which should be computed when this workbench is deactivated
-        """
-        App.Console.PrintMessage(translate("Log", "Workbench datamanager_wb de-activated.") + "\n")
 
 
 Gui.addWorkbench(DataManagerWorkbench())
