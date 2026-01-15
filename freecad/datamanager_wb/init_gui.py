e812e8ffba5102716d9c9b1c4d570650c14c29f7
Roy Wright <roy@wright.org>
2026-01-15 00:57:15 -0600

    refactor: move init_gui back to package root

--- freecad/datamanager_wb/init_gui.py
+++ freecad/datamanager_wb/init_gui.py
@@ -1,6 +1,22 @@
-"""Compatibility wrapper for FreeCAD workbench initialization."""
+"""
+FreeCAD datamanager workbench
+"""
 
-# Import for side effects: FreeCAD workbench registration occurs at import time.
-from .entrypoints import init_gui as _init_gui  # noqa: F401
+try:
+    import FreeCADGui as Gui  # pylint: disable=import-error
+except Exception:  # pylint: disable=broad-exception-caught
+    Gui = None
 
-__all__: list[str] = []
+if Gui is not None:
+    from .entrypoints.commands import register_commands
+    from .entrypoints.workbench import DataManagerWorkbench
+    from .freecad_version_check import check_python_and_freecad_version
+    from .resources import TRANSLATIONSPATH
+    from .ui.main_panel import get_main_panel
+
+    Gui.addLanguagePath(TRANSLATIONSPATH)
+    Gui.updateLocale()
+
+    check_python_and_freecad_version()
+    register_commands(get_main_panel)
+    Gui.addWorkbench(DataManagerWorkbench())
